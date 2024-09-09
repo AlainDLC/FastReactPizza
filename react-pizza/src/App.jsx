5cef84764014f10648c4e3b02ebbc79ff3ed4cbd
AlainDLC <jd_alain@hotmail.com>
2024-09-09 15:15:39 +0200

    added functions

--- react-pizza/src/App.jsx
+++ react-pizza/src/App.jsx
@@ -1,14 +1,18 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayOut from "./ui/AppLayOut";
+import Error from "./ui/Error";
 
 const router = createBrowserRouter([
   {
     element: <AppLayOut />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
@@ -17,22 +21,26 @@ const router = createBrowserRouter([
       {
         path: "/menu",
         element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
       },
       {
         path: "/cart",
         element: <Cart />,
+        errorElement: <Error />,
       },
-      {
-        path: "/cart",
-        element: <Cart />,
-      },
+
       {
         path: "/order/new",
         element: <CreateOrder />,
+        errorElement: <Error />,
+        action: createOrderAction,
       },
       {
         path: "/order/:orderId",
         element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
